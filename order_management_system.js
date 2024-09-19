c15fa3b0538b788916a5a5ad3d1f70f6b7a63d25
Morgan Jacob <jacobmmori@gmail.com>
2024-09-18 23:52:20 -0400

    Initialize orders array

--- order_management_system.js
+++ order_management_system.js
@@ -6,3 +6,10 @@ const Inventory = [
     { name: 'Cappuccino', price: 3, quantity: 8 },
     { name: 'Mocha', price: 4, quantity: 6 }
 ]
+
+// Task 2- Create an Orders Array of Order 
+
+
+let orders = []
+
+// Task 3- 
